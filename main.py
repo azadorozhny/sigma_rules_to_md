cc8bf562e48c6a4edfa2ce602987afbac5bd17e2
Artur Zadorozhny <artur.zadorozhny@teskalabs.com>
2022-05-31 14:41:48 +0200

    reworked pdf output

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from fpdf import FPDF
 import yaml
 from tomark import Tomark
 
+import markdown as markdownlib
+
 # path to sigma rules
 rules_path = sys.argv[1]
 
@@ -67,6 +69,29 @@ def yaml_list_to_md_page(yaml_list):
         final_string += rule_in_md
     return final_string
 
+def yaml_list_to_pdf_page(yaml_list):
+    rule_template = '''
+    Title: {title}
+    - description: {description}
+    - category: {parent}
+    - level: {level}
+    - id: {id}
+    '''
+    final_string = '''
+    List of covered sigma rules
+    '''
+    for i in yaml_list:
+        rule_in_md = rule_template.format(
+            title=i['title'],
+            description=i['description'],
+            parent=i['parent'],
+            level=i['level'],
+            id=i['id']
+        )
+        # print(rule_in_md)
+        final_string += rule_in_md
+    return final_string
+
 def text_to_pdf(text, filename):
     a4_width_mm = 210
     pt_to_mm = 0.35
@@ -83,7 +108,7 @@ def text_to_pdf(text, filename):
     splitted = text.split('\n')
 
     for line in splitted:
-        lines = textwrap.wrap(line, width_text)
+        lines = textwrap.wrap(line, int(width_text))
 
         if len(lines) == 0:
             pdf.ln()
@@ -92,6 +117,29 @@ def text_to_pdf(text, filename):
             pdf.cell(0, fontsize_mm, wrap, ln=1)
 
     pdf.output(filename, 'F')
+
+
+def html_to_pdf(source_html, output_filename):
+    from xhtml2pdf import pisa  # import python module
+
+    # Utility function
+    def convert_html_to_pdf(source_html, output_filename):
+        # open output file for writing (truncated binary)
+        result_file = open(output_filename, "w+b")
+
+        # convert HTML to PDF
+        pisa_status = pisa.CreatePDF(
+            source_html,  # the HTML to convert
+            dest=result_file)  # file handle to recieve result
+
+        # close output file
+        result_file.close()  # close output file
+
+        # return False on success and True on errors
+        return pisa_status.err
+
+    pisa.showLogging()
+    convert_html_to_pdf(source_html, output_filename)
 
 def write_to_file(markdown):
     with open('sigma_rules_summary.txt', 'w+') as final_file:
@@ -110,6 +158,11 @@ if __name__ == '__main__':
 
     write_to_file(markdown)
     print('Your markdown file is ready in this directory under "sigma_rules_summary.txt" name')
+
     if pdf_output == 'yes':
-        text_to_pdf(markdown, 'sigma_rules_summary.pdf')
+
+        temp_html = markdownlib.markdown(yaml_list_to_pdf_page(loaded_yaml))
+        html_to_pdf(temp_html, 'sigma_rules_summary.pdf')
+
         print('Your PDF file is ready in this directory under "sigma_rules_summary.pdf" name')
+
