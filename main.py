f2998ace9826f2aff2cec3f737b31925a705beba
Artur Zadorozhny <artur.zadorozhny@teskalabs.com>
2022-05-31 13:37:58 +0200

    added pdf output

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import sys
 from pathlib import Path
 
+import textwrap
+from fpdf import FPDF
+
 import yaml
 from tomark import Tomark
 
 # path to sigma rules
 rules_path = sys.argv[1]
 
+print('Page format extracts selected fields, table format prints all fields')
+md_format = input('Specify markdown format: (page or table): ')
+while md_format not in ['page', 'table']:
+    print('You have specified markdown format: ', md_format)
+    print('You have entered invalid option, type "page" or "table"')
+    md_format = input('Specify markdown format: (page or table): ')
+
+pdf_output = input('Do you want also a PDF output? ')
+while pdf_output not in ['yes','no']:
+    print('You have specified PDF option: ', pdf_output)
+    print('You have entered invalid option, type "yes" or "no"')
+    pdf_output = input('Specify if you want also a PDF output: (yes or no): ')
 
 def load_yaml(path_string):
     path_iterable = Path(path_string).rglob('*.yml')
@@ -43,11 +58,35 @@ def yaml_list_to_md_page(yaml_list):
             level=i['level'],
             id=i['id']
         )
-        print(rule_in_md)
+        # print(rule_in_md)
         final_string += rule_in_md
     return final_string
 
+def text_to_pdf(text, filename):
+    a4_width_mm = 210
+    pt_to_mm = 0.35
+    fontsize_pt = 10
+    fontsize_mm = fontsize_pt * pt_to_mm
+    margin_bottom_mm = 10
+    character_width_mm = 7 * pt_to_mm
+    width_text = a4_width_mm / character_width_mm
 
+    pdf = FPDF(orientation='P', unit='mm', format='A4')
+    pdf.set_auto_page_break(True, margin=margin_bottom_mm)
+    pdf.add_page()
+    pdf.set_font(family='Courier', size=fontsize_pt)
+    splitted = text.split('\n')
+
+    for line in splitted:
+        lines = textwrap.wrap(line, width_text)
+
+        if len(lines) == 0:
+            pdf.ln()
+
+        for wrap in lines:
+            pdf.cell(0, fontsize_mm, wrap, ln=1)
+
+    pdf.output(filename, 'F')
 
 def write_to_file(markdown):
     with open('sigma_rules_summary.txt', 'w+') as final_file:
@@ -57,11 +96,15 @@ def write_to_file(markdown):
 if __name__ == '__main__':
     loaded_yaml = load_yaml(rules_path)
 
-    # 2 options: page or table output
-    markdown = yaml_list_to_md_page(loaded_yaml)
-    # markdown = yaml_list_to_md_table(loaded_yaml)
+    markdown = None
+
+    if md_format == 'page':
+        markdown = yaml_list_to_md_page(loaded_yaml)
+    else:
+        markdown = yaml_list_to_md_table(loaded_yaml)
 
     write_to_file(markdown)
-
-
-
+    print('Your markdown file is ready in this directory under "sigma_rules_summary.txt" name')
+    if pdf_output == 'yes':
+        text_to_pdf(markdown, 'sigma_rules_summary.pdf')
+        print('Your PDF file is ready in this directory under "sigma_rules_summary.pdf" name')
